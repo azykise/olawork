661ee0822bfbde766580d15509a38f59a9a3a17b
azykise <azykise@163.com>
2013-01-18 11:55:47 +0800

    上传文档

---
+++ doc/reading_notes/tech/cshapr_coroutine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace csharp_coroutine
+{
+    class Program
+    {        
+        static IEnumerable<int> DoCoroutine()
+        {
+            int i = 0;
+
+            while(true)
+            {                
+                System.Console.WriteLine((i++).ToString());
+                yield return i;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IEnumerator<int> co = DoCoroutine().GetEnumerator();
+
+            co.MoveNext();
+            co.MoveNext();
+            co.MoveNext();
+
+            System.Console.ReadLine();
+        }
+    }
+}
